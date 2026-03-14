2b0c9cc32716f64e6f29ced5ab4cbfbc16685632
SamratGhimire01 <samratghimire01@gmail.com>
2026-03-14 21:42:43 +0545

    feat: add basic csv reading and cleaning logic

--- src/cleaner.py
+++ src/cleaner.py
@@ -1,13 +1,36 @@
 import csv
+import logging
+from datetime import datetime
+logging.basicConfig(
+    level=logging.ERROR, 
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("log.log"),
+        logging.StreamHandler()
+    ]
+)
+
+def parse_date(date_str):
+    try:
+        return datetime.strptime(date_str.strip(),"%Y-%m-%d")
+    except Exception as e:
+        logging.error(f"Date error: {e}")
+        return None
+def clean_row(row):
+    d1 = [_.strip() for _ in row]
+    if "" in d1:
+        logging.error(f"Null value found in row and removed: {row}")
+        return None
+    return d1
+
 def read_csv(data):
     with open(data, "r",newline='') as file:
         csv_read = csv.reader(file)
         with open("Updated_file.csv","w") as w_file:
             csv_w = csv.writer(w_file, delimiter=",")
             for line in csv_read:
-                d1=[_.strip() for _ in line]
-                if "" in d1:
-                    del line
+                d1=clean_row(line)
+                if d1 is None:
                     continue
                 csv_w.writerow(d1)
         
