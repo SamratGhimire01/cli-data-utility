df8834da2d20f2cc967aab65bd333428781eff26
SamratGhimire01 <samratghimire01@gmail.com>
2026-03-15 17:40:54 +0545

    updated the cleaner withe new discard_file.csv

--- src/cleaner.py
+++ src/cleaner.py
@@ -1,6 +1,7 @@
 import csv
 import logging
 from datetime import datetime
+
 logging.basicConfig(
     level=logging.ERROR, 
     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -12,36 +13,46 @@ logging.basicConfig(
 
 def parse_date(date_str):
     try:
-        return datetime.strptime(date_str.strip(),"%Y-%m-%d")
+        return datetime.strptime(date_str.strip(), "%Y-%m-%d")
     except Exception as e:
         logging.error(f"Date error for '{date_str}': {e}")
         return None
-def clean_row(row):
+
+def clean_row(row, discard_writer):
     d1 = [_.strip() for _ in row]
+    
     if "" in d1:
         logging.error(f"Null value found in row and removed: {row}")
+        discard_writer.writerow(row)
         return None
-    for i,field in enumerate(d1):
+        
+    for field in d1:
         if "-" in field and len(field) == 10:
             parsed = parse_date(field)
             if not parsed:
                 logging.error(f"Invalid date format in field '{field}' : '{row}'")
+                discard_writer.writerow(row)
                 return None
     return d1
 
-def read_csv(input_path, output_path= "Updated_file.csv"):
+def read_csv(input_path, output_path="Updated_file.csv"):
     try:
-        with open(input_path, "r",newline='') as file:
+        with open(input_path, "r", newline='') as file:
             csv_read = csv.reader(file)
-            with open(output_path,"w",newline='') as w_file:
+            
+            
+            with open(output_path, "w", newline='') as w_file, \
+                 open("discarded_data.csv", "w", newline='') as d_file:
+                
                 csv_w = csv.writer(w_file, delimiter=",")
+                csv_discard = csv.writer(d_file, delimiter=",")
+                
                 for line in csv_read:
-                    cleaned=clean_row(line)
-                    if cleaned :
+                    cleaned = clean_row(line, csv_discard)
+                    if cleaned:
                         csv_w.writerow(cleaned)
     except Exception as e:
         logging.error(f"File error: {e}")
-        
-        
+
 if __name__ == "__main__":
     read_csv("data.csv")
--- src/cli.py
+++ src/cli.py
@@ -5,5 +5,6 @@ def get_args():
     parse = ArgumentParser(description="CSV Cleaning and Date Parsing Tool")
     parse.add_argument("--input", required=True, help="Path to the input CSV file" )
     parse.add_argument("--output", default="Updated_file.csv", help="Path to save the cleaned CSV" )
+    parse.add_argument("--dfile", default="discarded_data.csv", help="Path to save the discarded CSV" )
     return parse.parse_args()
 
